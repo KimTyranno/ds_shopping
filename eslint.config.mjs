c1b13270ef4fa83834f17170fb2fb1f32eb074d8
kimTyranno <eotjdgkdl@gmail.com>
2025-08-10 17:57:02 +0900

    eslint 파일수정(.eslintrc.js -> eslint.config.msj)

---
+++ eslint.config.mjs
@@ -0,0 +1,48 @@
+import js from '@eslint/js'
+import next from 'eslint-config-next'
+import * as tseslint from 'typescript-eslint'
+
+export default [
+  // 기본 JS 권장 설정
+  js.configs.recommended,
+
+  // Next.js 권장 설정
+  ...next(),
+
+  // TypeScript 권장 설정
+  ...tseslint.configs.recommendedTypeChecked,
+
+  // 프로젝트별 세부 설정
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      'no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+  },
+
+  // JSX 관련 설정
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off', // Next.js에서 필요 없음
+      'no-undef': 'off', // 타입스크립트에서 관리
+    },
+  },
+]
