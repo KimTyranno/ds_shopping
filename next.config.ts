74e00ccf7a8c7ae7c36fe935c87c3920371c34ce
kimTyranno <eotjdgkdl@gmail.com>
2025-08-31 02:46:49 +0900

    body용량을 2MB로 설정

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,12 @@ const nextConfig: NextConfig = {
     // 이미지 때문에 임시로 허용
     domains: ['picsum.photos'],
   },
+  experimental: {
+    serverActions: {
+      // 용량제한이 기본적으로 1MB이므로 제한을 2MB로 변경
+      bodySizeLimit: '2mb',
+    },
+  },
 }
 
 const withNextIntl = createNextIntlPlugin()
