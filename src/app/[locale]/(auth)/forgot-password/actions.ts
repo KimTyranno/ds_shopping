db1d104f02d60f66d736c267b741a8ef7fb24568
kimTyranno <eotjdgkdl@gmail.com>
2025-09-05 18:48:03 +0900

    비밀번호 재설정 이메일전송 완료 UI 작성

--- src/app/[locale]/(auth)/forgot-password/actions.ts
+++ src/app/[locale]/(auth)/forgot-password/actions.ts
@@ -4,6 +4,7 @@ import { redirect } from '@/i18n/navigation'
 import { logger } from '@/lib/logger'
 import { createClient } from '@/lib/server'
 import { getLocale } from 'next-intl/server'
+import { cookies } from 'next/headers'
 
 export type ForgotPasswordState = {
   error?: string
@@ -42,6 +43,13 @@ export async function sendPasswordResetEmailAction(
         error: 'password_reset_email_failed',
       }
     }
+
+    const cookieStore = await cookies()
+    cookieStore.set('forgot_password_verified', 'true', {
+      maxAge: 60, // 60초 동안 유효
+      httpOnly: true, // 클라이언트 측 접근을 차단함
+      secure: true, // HTTPS 연결을 통해서만 쿠키가 전송되도록함
+    })
 
     // 성공시 확인 페이지로 리다이렉트
     redirect({
