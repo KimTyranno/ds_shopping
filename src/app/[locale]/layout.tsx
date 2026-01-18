cf7a96ae51a005991b060781ffc1da3638fc40a8
kimTyranno <eotjdgkdl@gmail.com>
2026-01-18 23:14:45 +0900

    PWA manifest 수정3

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -71,7 +71,10 @@ export default async function RootLayout({ children, params }: rootProps) {
       <head>
         {/* 절대경로 /manifest.json */}
         {/* PWA Manifest */}
-        <link rel="manifest" href="/manifest.webmanifest" />
+        <link
+          rel="manifest"
+          href="https://dsshopping.vercel.app/manifest.json"
+        />
         <meta name="theme-color" content="#000000" />
         {/* Apple 홈화면 아이콘 */}
         <link rel="apple-touch-icon" href="/icons/192.png" />
